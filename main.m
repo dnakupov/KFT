1b2c8bc30ab57a72bcc9e7c4957f30b246253af7
Daniyar Nakupov <dn@aidan-us.kz>
2011-03-02 20:40:05 +0600

    KFT Sources

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Kazakh Folk Tales - 1.1
+//
+//  Created by dnakupov on 02.02.11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"Kazakh_Folk_Tales___1_1AppDelegate");
+    [pool release];
+    return retVal;
+}
