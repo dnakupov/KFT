ee8f77ea63cada38679d235040938b8487ab3a66
Daniyar Nakupov <dn@aidan-us.kz>
2011-03-02 20:44:06 +0600

    KFT Sources

---
+++ Classes/Kazakh_Folk_Tales___1_1AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  Kazakh_Folk_Tales___1_1AppDelegate.h
+//  Kazakh Folk Tales - 1.1
+//
+//  Created by dnakupov on 02.02.11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface Kazakh_Folk_Tales___1_1AppDelegate : PhoneGapDelegate {
+}
+
+@end
+
